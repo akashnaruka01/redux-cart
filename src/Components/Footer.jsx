fb0c00f9180ba90eb8570219da1d8a0b433fb072
Akash Singh Naruka <akashnaruka01@gmail.com>
2023-08-18 18:56:35 +0530

    footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,26 @@
-import { Box, Button, HStack, Stack, Text } from '@chakra-ui/react'
-import React from 'react';
-import {AiFillFacebook, AiFillGithub, AiFillInstagram, AiFillLinkedin} from 'react-icons/ai';
-
+import { Box, Button, HStack, Heading, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  AiFillFacebook,
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <>
-    <hr />
-    <Box w="full" p={"4"} h="22vh"  className="footerBox">
+  return (
+    <>
+      <hr />
+      <Box w="full" p={"4"} h="22vh" className="footerBox">
         <Stack
-          className='footer'
+          className="footer"
           h={"70%"}
           color={"white"}
           justifyContent={"space-between"}
-          display={'flex'}
+          display={"flex"}
           flexDirection={"row"}
         >
-          <HStack w={"auto"} className="footerPart1" >
-           
+          <HStack w={"auto"} className="footerPart1">
             <Text
               letterSpacing={"widest"}
               lineHeight={"150%"}
@@ -24,59 +28,61 @@ const Footer = () => {
               textAlign={"center"}
               fontWeight={"500"}
               className="heading"
-              color={'#2874f0'}
-              p={'2'}
+              color={"#2874f0"}
+              p={"2"}
             >
               Shopping Cart
             </Text>
           </HStack>
           <HStack
             className="footerLine"
-            w={["0%", "container", "30%", "40%"]}
-            bgColor={"whiteAlpha.900"}
+            w={"60%"}
+            bgColor={"white"}
             m={"4"}
             h={"0.1"}
             alignSelf={"center"}
           ></HStack>
 
-          <HStack p={"4"} w={"auto"} justifyContent={'center'} >
-            <Button variant={"link"} bgColor={'inherit'} border={'0'}>
-              <a href="https://github.com/akashnaruka01">
-                <AiFillGithub size={"40"} color='grey'/>
-              </a>
-            </Button>
-            <Button variant={"link"} bgColor={'inherit'} border={'0'}>
-              <a href="https://www.linkedin.com/in/akash-singh-78897620b/">
-                <AiFillLinkedin size={"40"} color='grey'/>
-              </a>
-            </Button>
-            <Button variant={"link"} bgColor={'inherit'} border={'0'}>
-              <a href="https://www.instagram.com/">
-                <AiFillInstagram size={"40"} color='grey'/>
-              </a>
-            </Button>
-            <Button variant={"link"} bgColor={'inherit'} border={'0'}>
-              <a href="https://www.facebook.com">
-                <AiFillFacebook size={"40"} color='grey'/>
-              </a>
-            </Button>
-            <Button variant={"link"} bgColor={'inherit'} border={'0'}>
-              <a href="https://web.telegram.org/">
-                <AiFillLinkedin size={"40"}  color='grey'/>
-              </a>
-            </Button>
-          </HStack>
+          <Stack justifyContent={"center"} gap={"10px"} w={"20%"} className="socialMedia">
+            <Heading alignSelf={"center"} color={"gray"} className="socialMediaH">
+              Social Media
+            </Heading>
+
+            <HStack w={"auto"} justifyContent={"center"} gap={"20px"}>
+              <Button variant={"link"} bgColor={"inherit"} border={"0"}>
+                <a href="https://github.com/akashnaruka01">
+                  <AiFillGithub size={"40"} color="grey" />
+                </a>
+              </Button>
+              <Button variant={"link"} bgColor={"inherit"} border={"0"}>
+                <a href="https://www.linkedin.com/in/akash-singh-78897620b/">
+                  <AiFillLinkedin size={"40"} color="grey" />
+                </a>
+              </Button>
+              <Button variant={"link"} bgColor={"inherit"} border={"0"}>
+                <a href="https://www.instagram.com/prabhuu_000001/">
+                  <AiFillInstagram size={"40"} color="grey" />
+                </a>
+              </Button>
+              <Button variant={"link"} bgColor={"inherit"} border={"0"}>
+                <a href="https://www.facebook.com/akash.naruka.71/">
+                  <AiFillFacebook size={"40"} color="grey" />
+                </a>
+              </Button>
+            </HStack>
+          </Stack>
         </Stack>
         <Text
-        className="copyright"
+          className="copyright"
           color={"white"}
-          fontSize={["0.8rem", "0.9rem", "1.2rem"]}
+          fontSize={ "1.2rem"}
           px={"4"}
         >
           Copyright © 2023 Rookie.Crypto. All rights reserved.
         </Text>
       </Box>
-  </>
-}
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
